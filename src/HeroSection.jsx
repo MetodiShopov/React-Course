34e2980b12acdf5eeb1ac3110f0902d26cddfe78
Metodi <shopov.metodi@gmail.com>
2023-12-01 14:52:18 +0200

    Played arround with a Hero Image Section

---
+++ src/HeroSection.jsx
@@ -0,0 +1,14 @@
+export default function HeroSection(props) {(0)
+
+  return (
+    <>
+      <div className="hero-wrapper">
+        <h1>{props.title}</h1>
+        <img src="./src/assets/brooke-cagle-g1Kr4Ozfoac-unsplash.jpg" alt="image_one" />
+        <img src="./src/assets/joanna-kosinska-1_CMoFsPfso-unsplash.jpg" alt="image_two" />
+        <img src="./src/assets/marvin-meyer-SYTO3xs06fU-unsplash.jpg" alt="image_three" />
+        <img src="./src/assets/petr-sevcovic-qE1jxYXiwOA-unsplash.jpg" alt="image_four" />
+      </div>
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import HeroSection from './HeroSection.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <HeroSection title="Im the header now" />
   </React.StrictMode>,
 )
